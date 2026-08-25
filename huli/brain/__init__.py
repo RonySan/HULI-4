df22e3743026a817d175d23eca32443a94e73f00
RonySan <74458170+RonySan@users.noreply.github.com>
2026-08-26 04:36:01 +0700

    feat: export Intent Engine contracts

--- huli/brain/__init__.py
+++ huli/brain/__init__.py
@@ -1,1 +1,11 @@
 """Componentes cognitivos locais: intenção, contexto e planejamento."""
+
+from huli.brain.intent import IntentEngine, IntentMatch, IntentName
+from huli.brain.normalization import normalize_text
+
+__all__ = [
+    "IntentEngine",
+    "IntentMatch",
+    "IntentName",
+    "normalize_text",
+]
