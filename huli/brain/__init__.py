6d3f5991ffeccb4363ee98063814923d7c0bc6ed
RonySan <74458170+RonySan@users.noreply.github.com>
2026-08-26 23:06:30 +0700

    feat(brain): export dispatcher

--- huli/brain/__init__.py
+++ huli/brain/__init__.py
@@ -1,10 +1,12 @@
 """Componentes cognitivos locais: intenção, contexto e planejamento."""
 
+from huli.brain.dispatcher import BrainDispatcher
 from huli.brain.intent import IntentEngine, IntentMatch, IntentName
 from huli.brain.intent_events import IntentObserver
 from huli.brain.normalization import normalize_text
 
 __all__ = [
+    "BrainDispatcher",
     "IntentEngine",
     "IntentMatch",
     "IntentName",
