30d24ea6814ead3f7161c437645db3a42a51ef51
RonySan <74458170+RonySan@users.noreply.github.com>
2026-08-29 02:02:03 +0700

    fix(brain): route runtime through natural intent engine

--- huli/brain/__init__.py
+++ huli/brain/__init__.py
@@ -4,9 +4,23 @@ from huli.brain.agenda import AgendaService
 from huli.brain.context import ContextEngine, ContextSnapshot, ContextTurn
 from huli.brain.daily_summary import DailySummaryService
 from huli.brain.dispatcher import BrainDispatcher
-from huli.brain.intent import IntentEngine, IntentMatch, IntentName
+from huli.brain.intent import IntentMatch, IntentName
 from huli.brain.intent_events import IntentObserver
+from huli.brain.natural_intent import NaturalIntentEngine as IntentEngine
 from huli.brain.normalization import normalize_text
 from huli.brain.planner import PlannerService
 
-__all__ = ["AgendaService", "BrainDispatcher", "ContextEngine", "ContextSnapshot", "ContextTurn", "DailySummaryService", "IntentEngine", "IntentMatch", "IntentName", "IntentObserver", "PlannerService", "normalize_text"]
+__all__ = [
+    "AgendaService",
+    "BrainDispatcher",
+    "ContextEngine",
+    "ContextSnapshot",
+    "ContextTurn",
+    "DailySummaryService",
+    "IntentEngine",
+    "IntentMatch",
+    "IntentName",
+    "IntentObserver",
+    "PlannerService",
+    "normalize_text",
+]
